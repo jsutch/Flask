371f93a7ae3023ddc42f50ada822ea8ecd366bc2
root <jeff.sutch@acm.org>
2019-06-22 17:57:58 +0000

    added reg form to repo. fixed python 2 to 3 issues in ninja_gold and added wsgi definition

--- ninja_gold/server.py
+++ ninja_gold/server.py
@@ -6,40 +6,40 @@ app.secret_key = "MySecretKey" #necessary for session
 
 @app.route('/')
 def index():
-	# if gold not in session:
-	# 	session['gold'] = 0
-	# 	session['activity'] = []
-	# return render_template('index.html', activity_log=reversed(session['activity']))
-	return render_template('index.html')
+    # if gold not in session:
+    #     session['gold'] = 0
+    #     session['activity'] = []
+    # return render_template('index.html', activity_log=reversed(session['activity']))
+    return render_template('index.html')
 
 @app.route('/process', methods=['POST', 'GET'])
 def process():
-	print "PROCESSING"
-	activity_log = []
-	gold = 0
-	if not session['gold']:
-		session['gold'] = gold
-	else:	
-		if request.form['location'] == 'farm':
-			add_gold = random.randrange(10,20)
-		if request.form['location'] == 'cave':
-			add_gold = random.randrange(5,10)
-		if request.form['location'] == 'house':
-			add_gold = random.randrange(2,5)	
-		if request.form['location'] == 'casino':
-			add_gold = random.randrange(-50,50)	
-		print request.form['location'] , "added" , add_gold		
+    print("PROCESSING")
+    activity_log = []
+    gold = 0
+    if not session['gold']:
+        session['gold'] = gold
+    else:    
+        if request.form['location'] == 'farm':
+            add_gold = random.randrange(10,20)
+        if request.form['location'] == 'cave':
+            add_gold = random.randrange(5,10)
+        if request.form['location'] == 'house':
+            add_gold = random.randrange(2,5)    
+        if request.form['location'] == 'casino':
+            add_gold = random.randrange(-50,50)    
+        print(request.form['location'] , "added" , add_gold)       
 
-	session['gold']	+= add_gold
-	return render_template("index.html")
+    session['gold']    += add_gold
+    return render_template("index.html")
 
 
 @app.route('/reset', methods=['POST', 'GET'])
 def reset():
-	print "RESET"
-	session.clear()
-	return redirect('/')
+    print("RESET")
+    session.clear()
+    return redirect('/')
 
 
 if __name__ == "__main__":
-	app.run(debug=True)	
+    app.run(debug=True)    
---
+++ registration_form/README.md
@@ -0,0 +1,2 @@
+## Basic Registration Form stubs in Flask
+-added in wsgi hooks for gunicorn
